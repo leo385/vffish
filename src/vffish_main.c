5309b41dc60001cf8b2fd4d464255ecb8aff4344
Leonard Wieloch <leonsoshi@gmail.com>
2026-04-14 20:26:17 +0200

    Changes: - Prepared volk library in CMakeLists for faciliating Vulkan some API. - Initialize volk to make linking of vulkan lib. - Created check function to easy-handling Vulkan errors. - Created VkApplicationInfo and VkInstanceCreateInfo structures for app info and then attaching it to the engine.

--- src/vffish_main.c
+++ src/vffish_main.c
@@ -1,8 +1,22 @@
 #include <SDL3/SDL_init.h>
 #include <SDL3/SDL_video.h>
 #include <SDL3/SDL_events.h>
-#include <vulkan/vulkan.h>
+#include <volk/volk.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Application {
+		VkInstance instance;
+}Application;
+
+bool check(VkResult result) {
+	if(result == VK_ERROR_INITIALIZATION_FAILED) {
+		printf("Failed to initialize vulkan module, code: %d", result);
+		return 1;
+	}
+	
+	return 0;
+}
 
 int main(void) {
 
@@ -21,10 +35,46 @@ int main(void) {
             }
         }
 		
+		check(volkInitialize());
+		
 		// Vulkan code
 		uint32_t extensionCount = 0;
 		vkEnumerateInstanceExtensionProperties(NULL, &extensionCount, NULL);
-		printf("%d extensions have been found of vulkan\n", extensionCount);
+		
+		VkExtensionProperties *extensionProperties = malloc(extensionCount * sizeof(VkExtensionProperties));
+		const char** extensionNames = malloc(extensionCount * sizeof(const char*));
+		
+		if(extensionProperties != NULL) {
+			
+			vkEnumerateInstanceExtensionProperties(NULL, &extensionCount, extensionProperties);
+			printf("%d extensions have been found of vulkan\n", extensionCount);
+			
+			for(uint32_t i = 0; i < extensionCount; i++) {
+				
+				printf("Extension name: %s\n", extensionProperties[i].extensionName);
+				extensionNames[i] = extensionProperties[i].extensionName;
+			}
+			
+			free(extensionProperties);
+		}
+		
+		VkApplicationInfo appInfo;
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "vffish";
+		appInfo.applicationVersion = VK_MAKE_VERSION(0, 0, 1);
+		appInfo.engineVersion = VK_MAKE_VERSION(0, 0, 1);
+		appInfo.apiVersion = VK_API_VERSION_1_3;
+		
+		VkInstanceCreateInfo createInfo;
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+		createInfo.enabledExtensionCount = extensionCount;
+		createInfo.ppEnabledExtensionNames = extensionNames;
+		
+		Application *app = malloc(sizeof(Application));
+		
+		// Remember to free *app, *extensionNames;
+	
     }
 
     SDL_DestroyWindow(window);
