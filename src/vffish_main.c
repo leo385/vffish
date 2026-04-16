97cb57260bab6b971e67b709fa241c164f14d2fb
Leonard Wieloch <leonsoshi@gmail.com>
2026-04-16 22:38:06 +0200

    Changes: - Added VMA library to project due to better manage with allocating blocks. - Added CXX compiling method to CMakeLists.txt for compiling VMA implementation. - Created Vulkan instance and load it within volk with our engine.

--- src/vffish_main.c
+++ src/vffish_main.c
@@ -1,7 +1,9 @@
 #include <SDL3/SDL_init.h>
 #include <SDL3/SDL_video.h>
 #include <SDL3/SDL_events.h>
+#include <vulkan/vulkan.h>
 #include <volk/volk.h>
+#include <vma/vk_mem_alloc.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -11,7 +13,15 @@ typedef struct Application {
 
 bool check(VkResult result) {
 	if(result == VK_ERROR_INITIALIZATION_FAILED) {
-		printf("Failed to initialize vulkan module, code: %d", result);
+		printf("Failed to initialize vulkan module, code: %d\n", result);
+		return 1;
+	}
+	else if(result == VK_ERROR_LAYER_NOT_PRESENT) {
+		printf("Error: Layer not presented, code: %d\n", result);
+		return 1;
+	}
+	else if(result == VK_ERROR_EXTENSION_NOT_PRESENT) {
+		printf("Error: Extension not presented, code: %d\n", result);
 		return 1;
 	}
 	
@@ -35,7 +45,9 @@ int main(void) {
             }
         }
 		
-		check(volkInitialize());
+		if(volkInitialize() != VK_SUCCESS) {
+			return 1;
+		}
 		
 		// Vulkan code
 		uint32_t extensionCount = 0;
@@ -55,25 +67,39 @@ int main(void) {
 				extensionNames[i] = extensionProperties[i].extensionName;
 			}
 			
-			free(extensionProperties);
 		}
 		
-		VkApplicationInfo appInfo;
+		/*
+			Ensure Vk structures at the beginning are initialized with { 0 },
+			because vkCreateInstance is looking for structures, and then garbage
+			could be loaded into VkInstance, that may cause SEGMENTATION FAULT.
+		*/
+		VkApplicationInfo appInfo = {0};
 		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
 		appInfo.pApplicationName = "vffish";
 		appInfo.applicationVersion = VK_MAKE_VERSION(0, 0, 1);
 		appInfo.engineVersion = VK_MAKE_VERSION(0, 0, 1);
 		appInfo.apiVersion = VK_API_VERSION_1_3;
 		
-		VkInstanceCreateInfo createInfo;
+		VkInstanceCreateInfo createInfo = {0};
 		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 		createInfo.pApplicationInfo = &appInfo;
 		createInfo.enabledExtensionCount = extensionCount;
 		createInfo.ppEnabledExtensionNames = extensionNames;
 		
 		Application *app = malloc(sizeof(Application));
+		VkResult result;
+		if(app) {
+				check(result = vkCreateInstance(&createInfo, NULL, &app->instance));
+		}
+		
+		/* extensionProperties are no more needed due to instance was created */
+		free(extensionProperties);
+		free(extensionNames);
+		if(app && result == VK_SUCCESS) volkLoadInstance(app->instance);
 		
 		// Remember to free *app, *extensionNames;
+		free(app);
 	
     }
 
